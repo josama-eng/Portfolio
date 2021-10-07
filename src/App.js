1e9c9f3b8edf7c30c55d066cb8b302d7320dbfe9
josama-eng <samardzicjovana27@gmail.com>
2021-10-07 19:02:26 +0200

    Styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import AboutMe from "./pages/AboutMe";
+import GlobalStyle from "./components/GlobalStyle";
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutMe />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,29 +1,64 @@
 import React from "react";
 import me from "../img/me.jpg";
+import styled from "styled-components";
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>Defeats are only resting places</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               for future <span>victories</span>
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>Michael Idvorsky Pupin</h2>
-          </div>
+          </Hide>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo,
+            aspernatur.
+          </p>
         </div>
         <button>Contact me</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={me} alt="" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+//Styled components
 
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: #ffffff;
+`;
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  img {
+    width: 400px;
+    height: 80vh;
+    object-fit: cover;
+  }
+  flex: 1;
+  overflow: hidden;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 export default AboutSection;
