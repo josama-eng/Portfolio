40dd19e39d8273092de38a34a38e71cd91dad06d
josama-eng <samardzicjovana27@gmail.com>
2021-10-09 18:34:09 +0200

    Faq Sesction

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -4,10 +4,14 @@ import mail from "../img/mail.png";
 import linkedin from "../img/linkedin.png";
 import git from "../img/git.png";
 import twitter from "../img/twitter.png";
+import styled from "styled-components";
+import { About } from "../styles";
 const FaqSection = () => {
   return (
-    <div className="faq">
-      <h2>Get to know me better.</h2>
+    <Faq>
+      <h2>
+        Get to know me <span>better</span>
+      </h2>
       <div className="question">
         <h4>How do I start programming?</h4>
         <div className="answer">
@@ -18,6 +22,7 @@ const FaqSection = () => {
             ago I started learning every day, and it continues today.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Few things about me</h4>
@@ -28,6 +33,7 @@ const FaqSection = () => {
             Sci-Fi movies and spending time in nature.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Let's connect!</h4>
@@ -53,9 +59,37 @@ const FaqSection = () => {
             <a href="tel:+381648521768">+381-64-8521-768</a>
           </div>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 };
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -41,6 +41,7 @@ p {
 
 h4 {
     font-weight: bold;
+    font-size: 2rem;
 }
 span {
     font-weight: bold;
